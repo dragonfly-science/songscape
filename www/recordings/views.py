f8f47c544a514a160ec5681abd8607883e83dd8e
Edward Abraham <edward@dragonfly.co.nz>
2012-10-28 22:06:18 +1300

    Pagination

--- www/recordings/views.py
+++ www/recordings/views.py
@@ -1,6 +1,17 @@
 from recordings.models import Score
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def scores(request):
-    scores = Score.objects.filter(kiwi_index__gt=5, kiwi=None)
+    scores_list = Score.objects.filter(kiwi_index__gt=5, kiwi=None)
+    paginator = Paginator(scores_list, 1)
+    page = request.GET.get('page') or 1
+    try:
+        scores = paginator.page(page)
+    except PageNotAnInteger:
+        scores = paginator.page(1)
+    except EmptyPage:
+        scores = paginator.page(paginator.num_pages)
+
     return render(request, 'recordings/scores.html', {'scores':scores})
+
