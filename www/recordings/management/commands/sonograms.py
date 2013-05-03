560f3b193ccbac3d325eca39af971f58ff3dd01d
Edward Abraham <edward@dragonfly.co.nz>
2013-05-04 09:48:57 +1200

    sonograms being saved

--- www/recordings/management/commands/sonograms.py
+++ www/recordings/management/commands/sonograms.py
@@ -7,18 +7,15 @@ from django.core.files.images import ImageFile
 
 from recordings.models import Snippet
 
-
-def save_sonogram(snippet, NFFT=256):
-    clf()
-    filename = "%s-%s.png" % (snippet.recording.deployment.site.code, datetime.strftime(snippet.datetime, "%Y%m%d-%H%M%S"),)
-    specgram(snippet.get_audio(), NFFT=NFFT, Fs=snippet.recording.sample_rate, hold=False)
-    savefig(os.path.join(settings.SONOGRAM_DIR, filename))
-    snippet.sonogram = ImageFile(os.path.join(settings.SONOGRAM_DIR, filename))
-
 class Command(BaseCommand):
     def handle(self, *args, **options):
         snippets = Snippet.objects.filter(sonogram='')
         for snippet in snippets:
-            save_sonogram(snippet)
+            try:
+                snippet.save_sonogram()
+            except:
+                #Logging goes here
+                print '%s: %s' % (snippet, 'Error generating sonogram')
+                pass
 
         
