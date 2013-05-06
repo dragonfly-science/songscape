302dae1257299162231453fcdd2da32695579254
Edward Abraham <edward@haast.dragonfly.co.nz>
2013-05-06 23:08:27 +1200

    working on Haast

--- www/recordings/management/commands/load_recordings.py
+++ www/recordings/management/commands/load_recordings.py
@@ -1,8 +1,9 @@
 from datetime import datetime
-import os
+import os, sys
 from time import time, strptime, mktime
 import wave
-import contextlib
+from contextlib import closing
+#import logging
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
@@ -24,7 +25,6 @@ class Command(BaseCommand):
             for f in files:
                 if f.endswith('.wav'):
                     starttime = get_starttime(f)
-                    print recorder_code, f, starttime
                     try:
                         deployment = Deployment.objects.get(recorder__code=recorder_code, start__lt=starttime, end__gt=starttime)
                         path = os.path.join(root, f)
@@ -36,24 +36,36 @@ class Command(BaseCommand):
                                 recording = Recording.objects.get(sha1=recording.get_hash())
                             except Recording.DoesNotExist:
                                 recording.save()
+                            except KeyboardInterrupt:
+                                break
+                            except:
+                                print recorder_code, f, starttime, 'Error reading file', sys.exc_info()[0]
+                                continue
                         #Now generate the snippets
                         if not recording.snippets.count():
-                            f = wave.open(path, 'r')
                             try:
-                                frames = f.getnframes()
-                                rate = f.getframerate()
-                                length = frames/float(rate)
-                                snippet_length = 60
-                                snippet_overlap = 0
-                                snippet_minimum = 59.9
-                                seconds = 0
-                                while seconds + snippet_minimum < length: #Discard any snippets that are less than snippet_minimum long
-                                    offset = max(seconds - snippet_overlap, 0)
-                                    duration = min(snippet_length + 2*snippet_overlap, length - offset)
-                                    Snippet(recording=recording, offset=offset, duration=duration).save()
-                                    seconds += snippet_length
-                            finally:
-                                f.close()
+                                with closing(wave.open(path, 'r')) as w:
+                                    frames = w.getnframes()
+                                    rate = w.getframerate()
+                                    length = frames/float(rate)
+                                    snippet_length = 60
+                                    snippet_overlap = 0
+                                    snippet_minimum = 59.9
+                                    seconds = 0
+                                    while seconds + snippet_minimum < length: #Discard any snippets that are less than snippet_minimum long
+                                        offset = max(seconds - snippet_overlap, 0)
+                                        duration = min(snippet_length + 2*snippet_overlap, length - offset)
+                                        Snippet(recording=recording, offset=offset, duration=duration).save()
+                                        seconds += snippet_length
+                                    print recorder_code, f, starttime
+                            except KeyboardInterrupt:
+                                break
+                            except:
+                                print recorder_code, f, starttime, 'Error extracting snippet', sys.exc_info()[0]
                     except Deployment.DoesNotExist:
-                        print 'No matching deployment found'
+                        print recorder_code, f, starttime, 'No matching deployment found'
+                    except KeyboardInterrupt:
+                        break
+                    except:
+                        print recorder_code, f, starttime, 'Hmmm. Something weird here', sys.exc_info()[0]
                     
