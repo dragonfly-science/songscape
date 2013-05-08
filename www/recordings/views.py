ca8b0ca57d172c090e7adbf84127f607d13ae6dd
Christopher Knox <chris@dragonfly.co.nz>
2013-05-08 16:51:52 +1200

    tagging sort of works

--- www/recordings/views.py
+++ www/recordings/views.py
@@ -1,5 +1,5 @@
 import wave
-from recordings.models import Snippet, Score, Detector, Tag, Analysis, Deployment, Organisation
+from recordings.models import Snippet, Score, Detector, Tag, Analysis, Deployment, Organisation, Identification
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import StreamingHttpResponse, HttpResponse, HttpResponseRedirect
@@ -91,7 +91,7 @@ def snippet(request, id):
     return render(request, 'recordings/snippet.html', {'snippet': snippet, 'next_id': next_id, 'previous_id': previous_id})
 
 
-def scores(request, code, version, default_page=1, per_page=500):
+def scores(request, code, version, default_page=1, per_page=100):
     filters = _get_filters(request)
     order = _get_order(request)
     request_parameters = _get_parameters(request)
@@ -173,3 +173,72 @@ def analysis_list(request):
     return render(request,
                   'recordings/analysis_list.html',
                   {'analyses': Analysis.objects.all()})
+
+
+def analysis_detail(request, code):
+    analysis = Analysis.objects.get(code=code)
+    return render(request,
+                  'recordings/analysis_detail.html',
+                   {'analysis': analysis})
+
+
+def analysis_snippet(request, code, snippet_id):
+    snippets = request.session.get('snippets', [])
+    if int(snippet_id) in snippets:
+        index = snippets.index(int(snippet_id))
+        try:
+            next_id = snippets[index + 1]
+        except IndexError:
+            next_id = None
+        try:
+            previous_id = snippets[index - 1]
+        except IndexError:
+            previous_id = None
+    else:
+        next_id = None
+        previous_id = None
+
+    analysis = Analysis.objects.get(code=code)
+    snippet = Snippet.objects.get(id=snippet_id)
+
+    previous_identification = Identification.objects.filter(snippet_id__exact=snippet_id, user_id__exact=request.user.id)
+
+    id_before = previous_identification.count() > 0
+
+    if request.method == "POST":
+        true_tags = []
+        false_tags = []
+        for tag in analysis.tags.all():
+            if request.POST[tag.code] == 'true':
+                true_tags.append(tag)
+            else:
+                false_tags.append(tag)
+
+        iden = Identification(user = request.user, analysis=analysis, snippet=snippet, comment="")
+        iden.save()
+        iden.true_tags.add(*true_tags)
+        iden.false_tags.add(*false_tags)
+
+        return HttpResponseRedirect('/analysis/%s/%s' % (code, next_id))
+
+    return render(request, 
+                  'recordings/analysis_snippet.html', 
+                  {'snippet': snippet,
+                   'analysis': analysis,
+                   'next_id': next_id, 
+                   'id_before': id_before,
+                   'previous_id': previous_id})
+
+
+def analysis(request, code):
+    this_analysis = Analysis.objects.get(code=code)
+    sort_options = ['score', 'time', 'random']
+
+    return render(request,
+                  'recordings/analysis.html',
+                   {'sort_options': sort_options,
+                    'analysis': this_analysis})
+
+
+def analysis_next(request, code):
+    pass
--- www/urls.py
+++ www/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 from django.conf import settings
-#from django.views.generic.simple import redirect_to
+# from django.views.generic.simple import redirect_to
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
@@ -16,14 +16,18 @@ urlpatterns = patterns('',
      url(r'^tags$', 'www.recordings.views.tags', name='tags'),
      url(r'^analysis/create$', 'www.recordings.views.analysis_create', name='analysis_create'),
      url(r'^analysis$', 'www.recordings.views.analysis_list', name='analysis_list'),
+     url(r'^analysis/(?P<code>[\w-]+)/detail$', 'www.recordings.views.analysis_detail', name='analysis_detail'),
+     url(r'^analysis/(?P<code>[\w-]+)/(?P<snippet_id>\d+)$', 'www.recordings.views.analysis_snippet', name='analysis_snippet'),
+     url(r'^analysis/(?P<code>[\w-]+)$', 'www.recordings.views.analysis', name='analysis'),
+     url(r'^analysis/(?P<code>[\w-]+)/next', 'www.recordings.views.analysis_next', name='analysis_next'),
     (r'^admin/', include(admin.site.urls)),
-    (r'^login/$',   'django.contrib.auth.views.login',    {'template_name': 'account/login.html'}),
+    (r'^accounts/login/$',   'django.contrib.auth.views.login',    {'template_name': 'account/login.html'}),
     (r'^logout/$',  'django.contrib.auth.views.logout',   {'next_page': '/'}),
     (r'^accounts/change_password/$',  'django.contrib.auth.views.password_change', {'post_change_redirect' : '/'}),
     (r'^accounts/change_password/done/$', 'django.contrib.auth.views.password_change_done'),
-    #(r'^accounts/profile/$', redirect_to, {'url': '/'}),
+    # (r'^accounts/profile/$', redirect_to, {'url': '/'}),
     #(r'^accounts/login/$', redirect_to, {'url': '/login/'}),
-    # url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':settings.MEDIA_ROOT}),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':settings.MEDIA_ROOT}),
     # url(r'^play/(?P<id>\d+).wav', 'www.recordings.views.play_snippet', name='play')
 )
 urlpatterns += staticfiles_urlpatterns()
