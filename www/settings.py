8a01ef6f21adb52a3908e586ad85bfcddb06b8ae
Joel Pitt <joel@joelpitt.com>
2013-05-09 17:41:19 +1200

    add allowed hosts setting and give static a valid value

--- www/settings.py
+++ www/settings.py
@@ -42,11 +42,7 @@ MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media/')
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = '/media/'
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_DIR, '../static')
 
 STATICFILES_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
