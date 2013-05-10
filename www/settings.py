9b9a53bf57127afc43dec981ff1d431417c46006
Joel Pitt <joel@joelpitt.com>
2013-05-10 16:53:34 +1200

    Add django debug toolbar and limit snippet ordering to the current page

--- www/settings.py
+++ www/settings.py
@@ -83,6 +83,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'www.urls'
@@ -102,7 +103,10 @@ INSTALLED_APPS = (
 
     'recordings',
     'south',
+    'debug_toolbar',
 )
+
+INTERNAL_IPS = ('127.0.0.1',)
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
