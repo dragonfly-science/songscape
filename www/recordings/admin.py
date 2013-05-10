58ced90e6f048b883a39ec03627688c0abd9a285
Joel Pitt <joel@joelpitt.com>
2013-05-10 16:08:48 +1200

    Add some more admin classes to have idea of what exists in db

--- www/recordings/admin.py
+++ www/recordings/admin.py
@@ -1,22 +1,36 @@
 from django.contrib import admin
 
-from recordings.models import Organisation, Site, Recorder, Deployment, Score
+from recordings.models import Organisation, Site, Recorder, Deployment, Score, Detector, Analysis
 
 
 class OrganisationAdmin(admin.ModelAdmin):
     list_display = ('code', 'name',)
 admin.site.register(Organisation, OrganisationAdmin)
 
+
 class SiteAdmin(admin.ModelAdmin):
     list_display = ('code', 'latitude', 'longitude', 'altitude', 'description',)
 admin.site.register(Site, SiteAdmin)
 
+
 class RecorderAdmin(admin.ModelAdmin):
     list_display = ('code', 'organisation', 'comments',)
 admin.site.register(Recorder, RecorderAdmin)
+
 
 class DeploymentAdmin(admin.ModelAdmin):
     list_display = ('site', 'recorder', 'start', 'end', 'comments',)
     list_filter = ('site', 'recorder',)
 admin.site.register(Deployment, DeploymentAdmin)
 
+
+class DetectorAdmin(admin.ModelAdmin):
+    list_display = ('code', 'description', 'version',)
+    list_filter = ('code', 'signal',)
+admin.site.register(Detector, DetectorAdmin)
+
+
+class AnalysisAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'description', 'datetime',)
+    list_filter = ('organisation', )
+admin.site.register(Analysis, AnalysisAdmin)
