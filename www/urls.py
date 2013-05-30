c6d6294327ba800904bf31224fe7841abf1b86ae
Christopher Knox <chris@dragonfly.co.nz>
2013-05-30 13:38:57 +1200

    Added a summary page

--- www/urls.py
+++ www/urls.py
@@ -12,7 +12,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 #TODO: ISO 8601 datetime
 snippet_regex = r"(?P<organisation>[\w]+)-(?P<site_code>[\w]+)-(?P<date_time>\d+)-(?P<offset>[\d\.]+)-(?P<duration>[\d\.]+)-(?P<recorder_code>[\w]+)"
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^snippet/(?P<id>\d+)/$', 'www.recordings.views.snippet', name='snippet'),
     url(r'^snippet/%s/$' % snippet_regex, 'www.recordings.views.snippet', name='snippet_name'),
     url(r'^scores/(?P<code>[\w-]+)/(?P<version>[0-9\.]+)/$', 'www.recordings.views.scores', name='scores_list'),
@@ -23,19 +24,19 @@ urlpatterns = patterns('',
     url(r'^analysis/(?P<code>[\w-]+)/(?P<snippet_id>\d+)$', 'www.recordings.views.analysis_snippet', name='analysis_snippet'),
     url(r'^analysis/(?P<code>[\w-]+)$', 'www.recordings.views.analysis', name='analysis'),
     url(r'^analysis/(?P<code>[\w-]+)/next', 'www.recordings.views.analysis_next', name='analysis_next'),
+    url(r'^summary/$', 'www.recordings.views.summary', name='summary'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$',   'django.contrib.auth.views.login',    {'template_name': 'account/login.html'}),
     url(r'^logout/$',  'django.contrib.auth.views.logout',   {'next_page': '/'}),
-    url(r'^accounts/change_password/$',  'django.contrib.auth.views.password_change', {'post_change_redirect' : '/'}),
+    url(r'^accounts/change_password/$', 'django.contrib.auth.views.password_change', {'post_change_redirect': '/'}),
     url(r'^accounts/change_password/done/$', 'django.contrib.auth.views.password_change_done'),
     url(r'^$', 'www.recordings.views.home', name='home'),
     # (r'^accounts/profile/$', redirect_to, {'url': '/'}),
     #(r'^accounts/login/$', redirect_to, {'url': '/login/'}),
     url(r'^sonogram/(?P<id>\d+).png', 'www.recordings.views.get_sonogram', name='sonogram_id'),
     url(r'^sonogram/%s.png' % snippet_regex, 'www.recordings.views.get_sonogram', name='sonogram'),
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':settings.MEDIA_ROOT}),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     url(r'^play/(?P<id>\d+).wav', 'www.recordings.views.play_snippet', name='play'),
     url(r'^play/%s.wav' % snippet_regex, 'www.recordings.views.play_snippet', name='play_name')
 )
 urlpatterns += staticfiles_urlpatterns()
-    
