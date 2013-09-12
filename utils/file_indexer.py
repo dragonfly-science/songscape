0e554faf8daaf215ff07cfae97237bf5bfa61e1e
Edward Abraham <edward@dragonfly.co.nz>
2013-09-13 06:58:31 +1200

    Got a basic file indexer working

--- utils/file_indexer.py
+++ utils/file_indexer.py
@@ -1,18 +1,40 @@
 import argparse
+import os
+import hashlib
+import fnmatch
+import socket
 
 import pymongo
 from pymongo import MongoClient
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-h", "--host", help="database host", default="localhost")	
-parser.add_argument("-p", "--port", type=int, help="database port", default=27017)	
+parser.add_argument("directory", help="start directory", default=".")	
+parser.add_argument("-H", "--host", help="database host", default="localhost")	
+parser.add_argument("-P", "--port", type=int, help="database port", default=27017)	
+parser.add_argument("-f", "--filter", help="file pattern", default="*")	
+parser.add_argument("-s", "--server", help="server name", default="")	
 
 if __name__ == "__main__":
-	args = arser.parse_args()
+	args = parser.parse_args()
 	
 	client = MongoClient(args.host, args.port)	
-	db = client.songscape
+	db = client.test
 	collection = db.files
+
+print args.filter
+server = args.server or socket.gethostname()
+for root, directories, files in os.walk(args.directory):
+    for item in fnmatch.filter(files, args.filter):
+        path = os.path.abspath(os.path.join(root, item))
+        handle = open(path, 'rb')
+        digest = hashlib.md5()
+        for block in iter(lambda: handle.read(1024*1024), ""):
+            digest.update(block)
+        collection.update({'md5': digest.hexdigest()}, 
+            {'$addToSet': {'paths': '%s:%s' % (server, path)}},
+            upsert=True)
+            
+
 
 
 
