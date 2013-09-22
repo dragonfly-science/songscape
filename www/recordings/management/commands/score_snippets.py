622f81869dd1881e239214aeab87db71651f3636
Edward Abraham <edward@dragonfly.co.nz>
2013-09-23 10:52:21 +1200

    Make snippet scoring by complete recording, by random recording

--- www/recordings/management/commands/score_snippets.py
+++ www/recordings/management/commands/score_snippets.py
@@ -1,17 +1,20 @@
 import sys
+import time
+
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
-
-from recordings.models import Score, Recording, Snippet, Detector
 from kokako.score import Audio
 from kokako.detectors.kiwi import SimpleKiwi
 from kokako.detectors.intensity import Energy, LowEnergy, Amplitude
+
+from recordings.models import Score, Recording, Snippet, Detector
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         detectors = [SimpleKiwi(), Energy(), LowEnergy(), Amplitude()]
         db_detectors = []
+        now = time.time()
         for detector in detectors:
             try:
                 db_detectors.append(Detector.objects.get(code=detector.code, version=detector.version))
@@ -22,25 +25,28 @@ class Command(BaseCommand):
                 db_detectors[-1].save()
         detectors = zip(detectors, db_detectors)
         kiwi_detector = Detector.objects.get(code = 'simple-north-island-brown-kiwi')
-        snippets = Snippet.objects.exclude(scores__detector = kiwi_detector)
-        for snippet in snippets:
-            count = 0
-            for detector, db_detector in detectors:
-                try:
-                    audio = Audio(snippet.get_audio(), snippet.recording.sample_rate)
-                    score = detector.score(audio)
-                    if not count:   
-                        print snippet, score
+        recordings = Recording.objects.all().order_by('?')
+        for recording in recordings:
+            snippets = Snippet.objects.filter(recording=recording).exclude(scores__detector=kiwi_detector).order_by('offset')
+            for snippet in snippets:
+                count = 0
+                for detector, db_detector in detectors:
                     try:
-                        s = Score.objects.get(detector=db_detector, snippet=snippet) 
-                        s.delete()
-                    except Score.DoesNotExist:
-                        pass
-                    s = Score(detector=db_detector, snippet=snippet,
-                        score=score)
-                    s.save()
-                except KeyboardInterrupt:
-                    raise
-                except:
-                    print detector, snippet, 'Scoring failed', sys.exc_info()[0]
-                count += 1
+                        audio = Audio(snippet.get_audio(), snippet.recording.sample_rate)
+                        score = detector.score(audio)
+                        if not count:   
+                            print '%s %0.1f %0.1f' % (snippet, time.time() - now, score)
+                            now = time.time()
+                        try:
+                            s = Score.objects.get(detector=db_detector, snippet=snippet) 
+                            s.delete()
+                        except Score.DoesNotExist:
+                            pass
+                        s = Score(detector=db_detector, snippet=snippet,
+                            score=score)
+                        s.save()
+                    except KeyboardInterrupt:
+                        raise
+                    except:
+                        print detector, snippet, 'Scoring failed', sys.exc_info()[0]
+                    count += 1
