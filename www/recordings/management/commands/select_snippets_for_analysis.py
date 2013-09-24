fde8d9588d877ec2b2bb2d3c4010dc309db6fdba
Edward Abraham <edward@dragonfly.co.nz>
2013-09-24 14:47:27 +1200

    generate an analysis set

--- www/recordings/management/commands/select_snippets_for_analysis.py
+++ www/recordings/management/commands/select_snippets_for_analysis.py
@@ -2,7 +2,7 @@ import random
 
 from django.core.management.base import BaseCommand, CommandError
 
-from recordings.models import Snippet, Analysis, AnalysisSet
+from recordings.models import Snippet, Analysis, AnalysisSet, Detector, Score
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
@@ -24,7 +24,7 @@ class Command(BaseCommand):
         kiwi_snippets = unclipped_snippets.filter(scores__in=kiwi_scores)
         kiwi_snippets = zip(list(kiwi_snippets), ['Simple NIBK score higher than 25 ']*len(kiwi_snippets))
 
-        snippet_set = list(random_snippets) + list(kiwi_snippets)
+        snippet_set = random_snippets + kiwi_snippets
         random.shuffle(snippet_set)
             
         for snip, reason in snippet_set:
