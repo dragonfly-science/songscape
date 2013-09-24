c8a2e058250fff30083c2dce8644dfe8c1a70075
Joel Pitt <joel@joelpitt.com>
2013-09-24 14:44:14 +1200

    refactor fabfile and rely on dragonfab equivalents instead

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,15 @@
-import os, time
+import os
 
 from fabric.api import *
+
 # Set this before dragonfab imports, because it will then set up logging for you
 env.DEBUG=False
 
-local_dir = os.path.dirname(__file__)
+env.local_dir = os.path.dirname(__file__)
 
 # This will make all environments available as root fabric commands
 from dragonfab.env import *
-from dragonfab import deb
+from dragonfab import deb, database, utils
 
 # TODO: we need to define this for dumping from production
 env.databasedump = ''
@@ -20,63 +21,23 @@ env.databasedump = ''
 env.use_ssh_config = True
 
 # Package dir specifies where .deb file will go
-env.package_dir = os.path.join(local_dir, '..')
+env.package_dir = os.path.join(env.local_dir, '..')
 
 # This is the name of the deb pacakage that will be built, and where it ends up
 env.package_name = 'songscape'
 
 @task
-def database_dump():
-    """Copy of current database from the production server to dumps/latest.sql"""
-    with lcd(local_dir):
-        local('mkdir -p dumps')
-        if os.path.exists('dumps/latest.sql'):
-            local('mv dumps/latest.sql dumps/latest.sql.last')
-        sudo('mkdir -p /var/backups/dumps/')
-        local('echo pg_dump -h %(db_host)s -U %(db_user)s -p %(db_port)s' % env)
-
-        sudo('pg_dump -h %(db_host)s -U %(db_user)s -p %(db_port)s > /var/backups/dumps/latest.sql' % env)
-        get('/var/backups/dumps/latest.sql', 'dumps/latest.sql')
-
-@task
-def database_push():
-    "Recreate database from dumps/latest.sql."
-    # TODO refactor this to be more generic (but probably best to wait until using dragonfab)
-    # (could also use debconf settings instead of having to add into environments.py)
-    sudo('mkdir -p /var/backups/dumps/')
-    put('dumps/latest.sql', '/var/backups/dumps/latest.sql', use_sudo=True)
-    with settings(warn_only=True):
-        run('dropdb -h %(db_host)s -U %(db_user)s -p %(db_port)s %(db_name)s' % env)
-    run('createdb -h %(db_host)s -U %(db_user)s -p %(db_port)s -O %(db_user)s %(db_name)s' % env)
-    run('psql -h %(db_host)s -U %(db_user)s -p %(db_port)s -d %(db_name)s -f /var/backups/dumps/latest.sql' % env)
-
-@task
-def database_migrate():
-    "Run south migrations to upgrade database"
-    with cd(env.remote_path):
-        run("./www/manage.py syncdb --noinput")
-        run("./www/manage.py migrate --list") # Run this to get the state of things first
-        run("./www/manage.py migrate")
-
-@task
 def database_test_setup():
     "Setup database for testing"
     with cd(env.remote_path):
-        run("python www/manage.py test_setup")
-
-@task
-def apache_restart():
-    "Restart apache"
-    sudo('sleep 2')
-    # http://stackoverflow.com/questions/6379484/fabric-appears-to-start-apache2-but-doesnt
-    sudo('service apache2 restart', pty=False)
+        run("python manage.py test_setup")
 
 # TESTING
 @task
 def test():
-    with cd(env.remote_path), lcd(local_dir):
+    with cd(env.remote_path), lcd(env.local_dir):
         put('.coveragerc', '.coveragerc', use_sudo=True)
-        sudo('coverage run www/manage.py test')
+        sudo('coverage run manage.py test')
         sudo('coverage html --include=www\*')
         sudo('coverage xml --include=www\*')
 
@@ -84,6 +45,6 @@ def test():
 def fetch_results():
     """ Fetch the test results from the remote server.  """
     with cd(env.remote_path):
-        get('nosetests.xml', local_dir)
-        get('coverage.xml', local_dir)
-        get('htmlcov', local_dir)
+        get('nosetests.xml', env.local_dir)
+        get('coverage.xml', env.local_dir)
+        get('htmlcov', env.local_dir)
