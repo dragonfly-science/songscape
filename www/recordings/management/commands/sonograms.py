2e95ae1d84d29af74b19d9424eba828213be26fd
Edward Abraham <edward@dragonfly.co.nz>
2014-01-20 22:23:07 +1300

    populate sonograms

--- www/recordings/management/commands/sonograms.py
+++ www/recordings/management/commands/sonograms.py
@@ -1,20 +1,20 @@
 import sys
 from django.core.management.base import BaseCommand
 
-from www.recordings.models import Snippet
+from www.recordings.models import AnalysisSet
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        snippets = Snippet.objects.filter(sonogram='')
-        for snippet in snippets:
+        analysis_set = AnalysisSet.objects.all()
+        for a in analysis_set:
             try:
-                snippet.save_sonogram(replace=False)
-                sys.stdout.write('\r%s' % snippet )
+                a.snippet.save_sonogram()
+                sys.stdout.write('\r%s' % a.snippet )
                 sys.stdout.flush()
             except KeyboardInterrupt:
                 break
             except:
                 #Logging goes here
-                print '%s: %s (%s)' % (snippet, 'Error generating sonogram', 
+                print '%s: %s (%s)' % (a.snippet, 'Error generating sonogram', 
                     sys.exc_info()[0])
                 pass
