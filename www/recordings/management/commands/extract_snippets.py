9054eab22350482657b842271c207f0e3f8de7cd
Edward Abraham <edward@dragonfly.co.nz>
2014-02-25 23:28:40 +1300

    working on adding in the doc recordings

--- www/recordings/management/commands/extract_snippets.py
+++ www/recordings/management/commands/extract_snippets.py
@@ -1,10 +1,11 @@
 import csv, os
+import hashlib
 
 from django.core.management.base import BaseCommand
 from www.recordings.models import Snippet, Recording
 
 import wavy
-BASE_DIR = '/kiwi/nocturnal-calls/rfpt/snippets'
+BASE_DIR = '/kiwi/identifications/rfpt/snippets'
 class Command(BaseCommand):
     def handle(self, *args, **options):
         reader = csv.reader(open(args[0]))
@@ -14,9 +15,12 @@ class Command(BaseCommand):
                     path = row[0]
                     offset = float(row[1])
                     duration = float(row[2])
-                    recording = Recording.objects.get(path = path)
+                    md5 = hashlib.md5(open(path).read())
+                    recording = Recording.objects.get(md5 = md5)
                     snippet = Snippet.objects.get(recording=recording, offset=offset, duration=duration)
                     wavy.slice_wave(path, os.path.join(BASE_DIR, snippet.get_soundfile_name()), offset, duration, 8000)
+                except Recording.DoesNotExist:
+                    print "Can't find the recording on row %s at path %s" % (i, path)
                 except:
                     print 'Something went wrong on row %s with recording %s' % (i, path)
                     raise
