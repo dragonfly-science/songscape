bc911a7065aaa3a069c3184dd64c028ad5dfb8e1
Jason Preble <jhiikun@gmail.com>
2016-02-06 16:45:52 +1300

    CHANGED: working on weather recordings selection

--- select_weather_test_recordings.py
+++ select_weather_test_recordings.py
@@ -3,13 +3,13 @@ import os
 import panda as pd
 #for explanation of panda see http://stackoverflow.com/questions/16503560/read-specific-columns-from-csv-file-with-python-csv
 
-#get the filenames you want to copy out
-df = pd.read_csv(csv_file)
-saved_column = df.column_name #you can also use df['column_name']
+#get the filenames you want to copy out from a csv file
+df = pd.read_csv(csv_file) #change csv_file to match the path of the file you want to read from
+saved_column = df.filestring #you can also use df['column_name']
 
 #copy and move recordings to external drive
-source = os.listdir("/tmp/")
-destination = "/tmp/newfolder/"
+source = os.listdir("/tmp/") #change this to match the solardrive directory or wherever you'd like to copy files from
+destination = "/tmp/newfolder/" #change this to match the path of the drive you'd like to copy to
 for files in source:
     if files == saved_column:
         shutil.copy(files,destination)
