529b8016d6670eaab39b628e5b7be9d19eddcb29
Victor <vykanton@hotmail.com>
2016-03-04 15:35:58 +1300

    CHANGED: tracking local_settings.py

---
+++ local_settings.py
@@ -0,0 +1,31 @@
+# Be careful with this file, as it is used as a template for the debian
+# package setup. Feel free to copy it to local_settings.py and change/fill in
+# as you like though.
+
+DATABASES = {
+    'default': {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": 'songscape_dev',
+        "USER": 'birdsong',
+        "PORT": '5000',
+        "HOST": 'localhost',
+        "PASSWORD": 'golgeinEnImvaur',
+    }
+}
+# Change this to True if you are doing local development
+DEBUG = True
+
+SITE_URL = ''
+MEDIA_ROOT = '/kiwi'
+
+EMAIL_HOST = ''
+SERVER_EMAIL = ''
+
+SESSION_COOKIE_SECURE = True
+ALLOWED_HOSTS = ['.dragonfly.co.nz']
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
+BROWSERID_AUDIENCES = ['https://songscape-staging.dragonfly.co.nz',]
+
+# generate this per server, and add to debconf
+# python -c 'import random; print "".join([random.choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)])'
+SECRET_KEY = '1^8)k29v9fm)=oy1exyl_#cnr0mw=^mx@j()5rshb+*b(70fbg'
